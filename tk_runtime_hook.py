cf2674758f9a6d7daed10345eabc4c2e67015d83
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2025-02-12 15:15:20 +1100

    Clean-up

--- tk_runtime_hook.py
+++ tk_runtime_hook.py
@@ -5,8 +5,8 @@ import sys
 def _fix_tkinter():
     if sys.platform == 'darwin':
         base_dir = os.path.dirname(os.path.dirname(os.path.dirname(sys.executable)))
-        os.environ['TK_LIBRARY'] = os.path.join(base_dir, 'Frameworks', 'tk.framework', 'Versions', 'Current', 'Resources')
-        os.environ['TCL_LIBRARY'] = os.path.join(base_dir, 'Frameworks', 'tcl.framework', 'Versions', 'Current', 'Resources')
+        os.environ['TK_LIBRARY'] = os.path.join(base_dir, 'Frameworks', 'tk', 'Resources')
+        os.environ['TCL_LIBRARY'] = os.path.join(base_dir, 'Frameworks', 'tcl', 'Resources')
         os.environ['TKPATH'] = os.path.join(base_dir, 'Frameworks')
 
 _fix_tkinter()
