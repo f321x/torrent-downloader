10eeba0abb24d71118f28903d354cdf91cf21449
Steven Yan <yulong.yan@uqconnect.edu.au>
2025-02-16 01:53:57 +1100

    chore: bump version to 1.1.0 for both packages

--- torrent-downloader-python/setup.py
+++ torrent-downloader-python/setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import sys
+
+# Define platform-specific dependencies
+if sys.platform == 'win32':
+    libtorrent_requires = ['libtorrent>=2.0.0']
+else:
+    # On macOS and Linux, libtorrent should be installed via system package manager
+    libtorrent_requires = []
 
 setup(
-    name='TorrentDownloader',
-    version='1.0.0',
+    name="torrent-downloader-python",
+    version="1.1.0",
     description='A Torrent Downloader with a graphical user interface built using Tkinter and libtorrent',
     author='Your Name',
     author_email='your_email@example.com',
     py_modules=['torrent_downloader_gui'],
     install_requires=[
         'python-libtorrent'
-    ],
+    ] + libtorrent_requires,
     entry_points={
         'console_scripts': [
             'torrent-downloader = torrent_downloader_gui:main'
